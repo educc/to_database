0b103a29f6c0abcfb5f557dc91f64aac6fdf965e
educc <edward1738@gmail.com>
2017-08-08 21:23:17 -0500

    'newfeature'

--- todb.py
+++ todb.py
@@ -3,26 +3,30 @@ import json
 from datetime import datetime
 from sqlalchemy.orm import Session
 from database import engine, Course, Topic, Exam, Question, QuestionDetail, ExamQuestion
+import base64
 
-
-def create_course():
+def create_course(data):
     return Course(
-        name="Course1", 
+        name=data["course_name"], 
+        courseCode=data["course_code"],
+        cost=data['course_cost'],
         description="created by todb.py",
         createdBy="todb.py",
+        language="EN",
+        courseStatusTypeId="ACTIVE",
         createdDate=datetime.utcnow(),
         deleted=0,
         version=0)
 
-def create_exam(mycourse):
+def create_exam(mycourse,data):
     return Exam(
         course = mycourse,
-        name="Exam1", 
+        name=data['exam_name'],
+        examCode=data['exam_code'],
         description="created by todb.py",
-        examCode="some code",
         examApprovalTerm=100,
         examApprovalType="FIX.PERC",
-        examStatusTypeId="ACTIVO",
+        examStatusTypeId="ACTIVE",
         questions=0,
         duration=30,
         createdBy="todb.py",
@@ -58,7 +62,7 @@ def create_question(mytopic, question_data):
         feedback = question_data['feedback']
     return Question(
             topic = mytopic,
-            description = question_data['question'].strip(),
+            description = get_question_desc(question_data['question']),
             questionLevelTypeId = "BASICO",
             questionTypeId = None,
             descriptionAnswer = feedback ,
@@ -84,30 +88,25 @@ def create_question_options(myquestion,options_data):
         xlist.append(option_item)
     return xlist
 
+def get_question_desc(questionstr:str):
+    result = questionstr.strip()
+    if result.startswith('base64,'):
+        allbytes = bytes(result[7:], "utf-8")
+        result = base64.decodebytes(allbytes)
+    return result
+
+
 def import_data(data):
-    """
-        example of 'data' argument
-        [
-            {
-                "options":[
-                    str,
-                    str
-                ],
-                "question": str,
-                "correct":str,
-                "feedback": str
-            },
-    """
-    
+    """  require a specific json format, see example.json """
     session = Session(engine)
 
     try:
-        course = create_course()
-        exam = create_exam(course)
+        course = create_course(data)
+        exam = create_exam(course, data)
         topic = create_topic(course)
 
         nquestions = 0
-        for question_obj in data:
+        for question_obj in data['questions']:
             nquestions += 1
             question = create_question(topic,question_obj)
 
